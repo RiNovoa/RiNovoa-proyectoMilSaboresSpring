d2659d1af5d0ae47f9e7f4be8d80da50340d698c
PAAWSA02LC1024 <Duoc@PAAWSA02LC1024.duoc.acad>
2025-11-07 11:41:34 -0300

    Estructura inicial

---
+++ src/main/java/com/pasteleria/pasteleria/PasteleriaApplication.java
@@ -0,0 +1,13 @@
+package com.pasteleria.pasteleria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteleriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteleriaApplication.class, args);
+	}
+
+}
